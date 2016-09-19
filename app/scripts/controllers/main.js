7a5f6e3b180bdc4197004227d81bd19d91535e32
wancy86 <wancy86@sina.com>
2016-09-19 23:05:08 +0800

    formaty

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('chatApp')
-  .controller('MainCtrl', function ($scope, chatroom) {
+
+.controller('MainCtrl', function($scope, chatroom) {
     $scope.getMessages = chatroom.getMessages;
     $scope.getVisitors = chatroom.getVisitors;
 
-    $scope.sendMessage = function () {
-      if(!$scope.newMessage) {
-        return;
-      }
-      chatroom.sendMessage($scope.newMessage);
-      $scope.newMessage = '';
+    $scope.sendMessage = function() {
+        if (!$scope.newMessage) {
+            return;
+        }
+        chatroom.sendMessage($scope.newMessage);
+        $scope.newMessage = '';
     };
-  });
+});
--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,36 +1,37 @@
 'use strict';
 
 angular.module('chatApp')
-  .controller('SignupCtrl', function ($scope, Auth, $location, mySocket) {
+
+.controller('SignupCtrl', function($scope, Auth, $location, mySocket) {
     $scope.user = {};
     $scope.errors = {};
 
     $scope.register = function(form) {
-      $scope.submitted = true;
-  
-      if(form.$valid) {
-        Auth.createUser({
-          name: $scope.user.name,
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Reconnect to socket as logged in user
-          mySocket.reconnect();
+        $scope.submitted = true;
 
-          // Account created, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          err = err.data;
-          $scope.errors = {};
+        if (form.$valid) {
+            Auth.createUser({
+                    name: $scope.user.name,
+                    email: $scope.user.email,
+                    password: $scope.user.password
+                })
+                .then(function() {
+                    // Reconnect to socket as logged in user
+                    mySocket.reconnect();
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.type;
-          });
-        });
-      }
+                    // Account created, redirect to home
+                    $location.path('/');
+                })
+                .catch(function(err) {
+                    err = err.data;
+                    $scope.errors = {};
+
+                    // Update validity of form fields that match the mongoose errors
+                    angular.forEach(err.errors, function(error, field) {
+                        form[field].$setValidity('mongoose', false);
+                        $scope.errors[field] = error.type;
+                    });
+                });
+        }
     };
-  });
+});
